fe48f1711845083887353e1da75d5cc4ca47dc12
Maximilian Anzinger <44003963+MaximilianAnzinger@users.noreply.github.com>
2022-11-07 18:44:02 +0100

    Update Tut02.thy

--- tutorial/Tut02.thy
+++ tutorial/Tut02.thy
@@ -10,18 +10,18 @@ fun deduplicate :: "'a list \<Rightarrow> 'a list"  where
 value "deduplicate [1,1,2,3,2,2,1::nat] = [1,2,3,2,1]"
 
 lemma "length (deduplicate xs) \<le> length xs"
-  apply(induction xs rule: deduplicate.induct) by auto
+  by(induction xs rule: deduplicate.induct, auto)
 
 fun subst :: "vname \<Rightarrow> aexp \<Rightarrow> aexp \<Rightarrow> aexp"  where
   "subst x a' (N n) = (N n)" |
   "subst x a' (V v) = (if x = v then a' else V v)" |
   "subst x a' (Plus i j) = Plus (subst x a' i) (subst x a' j)"
 
-lemma subst_lemma: "aval (subst x a' a) s = aval a (s(x := aval a' s))"
-  apply(induction a arbitrary: x a' s) by auto
+lemma subst_lemma[simp]: "aval (subst x a' a) s = aval a (s(x := aval a' s))"
+  by(induction a arbitrary: x a' s, auto)
 
 lemma comp: "aval a1 s = aval a2 s \<Longrightarrow> aval (subst x a1 a) s = aval (subst x a2 a) s"
-  apply(induction a arbitrary: x a1 a2 s) using subst_lemma by auto
+  by(induction a arbitrary: x a1 a2 s, auto)
 
 datatype aexp' = N' int | V' vname | Plus' aexp' aexp' | PI' vname
 
@@ -29,8 +29,10 @@ datatype aexp' = N' int | V' vname | Plus' aexp' aexp' | PI' vname
 fun aval' :: "aexp' \<Rightarrow> state \<Rightarrow> (val \<times> state)" where
   "aval' (N' n) s = (n, s)" |
   "aval' (V' v) s = (s v, s)" |
-  "aval' (Plus' i j) s = (case aval' i s of (i', s') \<Rightarrow>
-                        (case aval' j s' of (j', s'') \<Rightarrow> (i' + j', s'')))" |
+  "aval' (Plus' i j) s = (
+    let (i', s) = aval' i s;
+    (j', s) = aval' j s
+    in (i' + j', s))" |
   "aval' (PI' v) s = (s v, s(v := (s v) + 1))"
 
 value "<>(x := 0)"
@@ -41,14 +43,12 @@ lemma aval'_PI'_inc:
   "aval' (PI' v) s = (r, s') \<Longrightarrow> s x \<le> s' x"
   by auto
 
-lemma aval'_all_inc:
+lemma aval'_mono:
   "aval' a s = (r, s') \<Longrightarrow> s x \<le> s' x"
-  apply(induction a arbitrary: x s)
-  apply(auto split: prod.splits) 
+  by(induction a arbitrary: s r s', auto split: prod.splits, fastforce)
 
 lemma aval'_inc:
   "aval' a <> = (v, s') \<Longrightarrow> 0 \<le> s' x"
-  apply(induction a)
-  apply(auto simp: null_state_def split: prod.splits)
+  using aval'_mono by (auto simp: null_state_def)
 
 end
